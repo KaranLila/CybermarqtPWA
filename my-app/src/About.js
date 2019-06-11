5952169e5e8c36b45cdd43dbf2194b0972b880a8
KaranLila <karanlila@outlook.com>
2019-06-11 21:32:26 +0200

    First commit

---
+++ my-app/src/About.js
@@ -0,0 +1,24 @@
+import React  from 'react';
+
+export const About = () => (
+    <div>
+        <h2>About us</h2>
+        <p> 
+            Cybermarqt is the global marketplace and a reliable matchmaker for cyber security. 
+            Our mission is to make the cyber security world more transparent.
+        </p>
+
+        <p>
+            Cybermarqt offers a unique marketplace where providers of cyber security solutions advertise their services with a widespread global
+            reach and customers are offered an easy way to search for solutions worldwide. We enable customers and providers to find each other
+            and connect by using our website and matchmaking tool.
+        </p>
+
+        <p>
+            
+            By globally connecting people, knowledge and solutions, we centralize the cyber security market and make it more accessible.
+      
+        </p>
+    </div>
+
+)
---
+++ my-app/src/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Home = () => (
+    <div>
+        <h2>Cybermarqt- Search it.Find it. Secure it.</h2>
+        <p>Cybermarqt is the global marketplace and matchmaker for cyber security</p>
+    </div>
+
+)

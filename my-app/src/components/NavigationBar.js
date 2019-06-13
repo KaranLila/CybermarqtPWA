f87d947e6c30e71532cd54f7212bc90effe150bb
Zina <zina_1905@hotmail.com>
2019-06-13 17:01:51 +0200

    changesss
    changes

--- my-app/src/components/NavigationBar.js
+++ my-app/src/components/NavigationBar.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 const Styles = styled.div`
     .navbar {
-        background-color: #222;
+        background-color: #999;
     }
 
      a.navbar-brand, a.navbar-nav, a.nav-link{
@@ -23,10 +23,10 @@ const Styles = styled.div`
 export const NavigationBar = () => (
     <Styles>
         <Navbar expand ="lg">
-            <Navbar.Brand href="/">Cybermarqt</Navbar.Brand>
+            <Navbar.Brand href="/"></Navbar.Brand>
             <Navbar.Toggle aria-controls ="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
+                <Nav className="d-flex flex-column">
                     <Nav.Item><Nav.Link href ="/">Home</Nav.Link></Nav.Item>
                     <Nav.Item><Nav.Link href ="/about">About</Nav.Link></Nav.Item>
                     <Nav.Item><Nav.Link href ="/contact">Contact</Nav.Link></Nav.Item>
@@ -34,4 +34,5 @@ export const NavigationBar = () => (
             </Navbar.Collapse>
         </Navbar>
     </Styles>
+    
 )
